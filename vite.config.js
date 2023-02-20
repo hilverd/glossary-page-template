2999ad88720291b8e3e30bcd2b21a2eb215ee501
Hilverd Reker <hilverd@hilverdreker.com>
2023-02-20 12:08:28 +0000

    Add Python example glossary to Vite config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
                 index: path.resolve(__dirname, 'index.html'),
                 glossary: path.resolve(__dirname, 'glossary.html'),
                 empty: path.resolve(__dirname, 'empty.html'),
+                "python-3.11.2-glossary": path.resolve(__dirname, 'examples/python-3.11.2-glossary.html'),
                 "uk-parliament": path.resolve(__dirname, 'examples/uk-parliament.html'),
                 "open-glossary-of-edge-computing": path.resolve(__dirname, 'examples/open-glossary-of-edge-computing.html'),
                 "canadas-weather-and-meterology-glossary.html": path.resolve(__dirname, 'examples/canadas-weather-and-meterology-glossary.html')
