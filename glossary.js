b3fc1e9150e65a3ba6b16af00a09b1002af0c490
Hilverd Reker <hilverd@hilverdreker.com>
2023-06-27 21:21:12 +0100

    Make glossary.js handle absense of #glossary-page-container
    This could be considered a bit of a hack to make index.html not throw an
    error.

--- glossary.js
+++ glossary.js
@@ -5,225 +5,228 @@ import '@webcomponents/custom-elements';
 const katexIsAvailable = typeof katex != "undefined";
 
 const containerElement = document.getElementById('glossary-page-container');
-const containerDataset = containerElement.dataset;
 
-const enableHelpForMakingChanges = containerDataset.enableHelpForMakingChanges === 'true';
-const enableSavingChangesInMemory = containerDataset.enableSavingChangesInMemory === 'true';
-const enableMarkdownBasedSyntax = containerDataset.enableMarkdownBasedSyntax === 'true';
-const enableExportMenu = containerDataset.enableExportMenu !== 'false';
-const cardWidth = containerDataset.cardWidth;
-const enableLastUpdatedDates = containerDataset.enableLastUpdatedDates === 'true';
-const editorIsRunning = containerDataset.editorIsRunning === 'true';
+if (containerElement) {
+    const containerDataset = containerElement.dataset;
 
-const titleElement = document.getElementById('glossary-page-title');
-const aboutElement = document.getElementById('glossary-page-about');
+    const enableHelpForMakingChanges = containerDataset.enableHelpForMakingChanges === 'true';
+    const enableSavingChangesInMemory = containerDataset.enableSavingChangesInMemory === 'true';
+    const enableMarkdownBasedSyntax = containerDataset.enableMarkdownBasedSyntax === 'true';
+    const enableExportMenu = containerDataset.enableExportMenu !== 'false';
+    const cardWidth = containerDataset.cardWidth;
+    const enableLastUpdatedDates = containerDataset.enableLastUpdatedDates === 'true';
+    const editorIsRunning = containerDataset.editorIsRunning === 'true';
 
-const glossaryElement = document.getElementById('glossary-page-items');
+    const titleElement = document.getElementById('glossary-page-title');
+    const aboutElement = document.getElementById('glossary-page-about');
 
-const aboutParagraph = aboutElement.querySelector('p').textContent.trim();
+    const glossaryElement = document.getElementById('glossary-page-items');
 
-const aboutUlElement = aboutElement.querySelector('ul');
-const aboutLiElements = Array.prototype.slice.apply(aboutUlElement.querySelectorAll('li'));
-const aboutLinks = aboutLiElements.map(aboutLinkFromLiElement);
+    const aboutParagraph = aboutElement.querySelector('p').textContent.trim();
 
-const dlElement = glossaryElement.querySelector('dl');
-const glossaryItemDivElements = Array.prototype.slice.apply(dlElement.querySelectorAll('div'));
-const glossaryItems = glossaryItemDivElements.map(glossaryItemFromDivElement);
+    const aboutUlElement = aboutElement.querySelector('ul');
+    const aboutLiElements = Array.prototype.slice.apply(aboutUlElement.querySelectorAll('li'));
+    const aboutLinks = aboutLiElements.map(aboutLinkFromLiElement);
 
-function normaliseWhitespace(s) {
-    return s.replace(/\s+/g, ' ').trim();
-}
+    const dlElement = glossaryElement.querySelector('dl');
+    const glossaryItemDivElements = Array.prototype.slice.apply(dlElement.querySelectorAll('div'));
+    const glossaryItems = glossaryItemDivElements.map(glossaryItemFromDivElement);
 
-function aboutLinkFromLiElement(aboutLiElement) {
-    const aElement = aboutLiElement.querySelector('a');
-    return {
-        href: aElement.getAttribute('href'),
-        body: normaliseWhitespace(aElement.textContent)
+    function normaliseWhitespace(s) {
+        return s.replace(/\s+/g, ' ').trim();
     }
-}
 
-function glossaryItemFromDivElement(glossaryItemDivElement) {
-    const dtElements = Array.prototype.slice.apply(glossaryItemDivElement.querySelectorAll('dt'));
-    const ddElements = Array.prototype.slice.apply(glossaryItemDivElement.querySelectorAll('dd'));
-    const relatedTermDdElements = ddElements.filter(ddElement => ddElement.getAttribute('class') === 'related-terms');
-    const relatedTerms = (relatedTermDdElements.length > 0) ? glossaryItemRelatedTermFromDdElement(relatedTermDdElements[0]) : [];
-    const needsUpdatingDdElements = ddElements.filter(ddElement => ddElement.getAttribute('class') === 'needs-updating');
-    const lastUpdatedDate = glossaryItemDivElement.dataset.lastUpdated;
-
-    return {
-        terms: dtElements.map(glossaryItemTermFromDtElement),
-        details: ddElements
-            .filter(ddElement => ddElement.getAttribute('class') !== 'related-terms' && ddElement.getAttribute('class') !== 'needs-updating')
-            .map(ddElement => ddElement.textContent.trim()),
-        relatedTerms: relatedTerms,
-        needsUpdating: needsUpdatingDdElements.length > 0,
-        lastUpdatedDate: lastUpdatedDate
+    function aboutLinkFromLiElement(aboutLiElement) {
+        const aElement = aboutLiElement.querySelector('a');
+        return {
+            href: aElement.getAttribute('href'),
+            body: normaliseWhitespace(aElement.textContent)
+        }
     }
-}
 
-function glossaryItemTermFromDtElement(dtElement) {
-    const dfnElement = dtElement.querySelector('dfn');
-    const id = dfnElement.id || null;
-    const isAbbreviation = Boolean(dfnElement.querySelector('abbr'));
-    const body = dfnElement.textContent;
-
-    return {
-        id: normaliseWhitespace(id),
-        isAbbreviation: isAbbreviation,
-        body: normaliseWhitespace(body)
-    };
-}
-
-function glossaryItemRelatedTermFromDdElement(ddElement) {
-    return Array.prototype.slice.apply(ddElement.querySelectorAll('a')).map(aElement => {
-        const hrefAttribute = aElement.getAttribute('href');
+    function glossaryItemFromDivElement(glossaryItemDivElement) {
+        const dtElements = Array.prototype.slice.apply(glossaryItemDivElement.querySelectorAll('dt'));
+        const ddElements = Array.prototype.slice.apply(glossaryItemDivElement.querySelectorAll('dd'));
+        const relatedTermDdElements = ddElements.filter(ddElement => ddElement.getAttribute('class') === 'related-terms');
+        const relatedTerms = (relatedTermDdElements.length > 0) ? glossaryItemRelatedTermFromDdElement(relatedTermDdElements[0]) : [];
+        const needsUpdatingDdElements = ddElements.filter(ddElement => ddElement.getAttribute('class') === 'needs-updating');
+        const lastUpdatedDate = glossaryItemDivElement.dataset.lastUpdated;
 
         return {
-            idReference: hrefAttribute.substring(1),
-            body: normaliseWhitespace(aElement.textContent)
+            terms: dtElements.map(glossaryItemTermFromDtElement),
+            details: ddElements
+                .filter(ddElement => ddElement.getAttribute('class') !== 'related-terms' && ddElement.getAttribute('class') !== 'needs-updating')
+                .map(ddElement => ddElement.textContent.trim()),
+            relatedTerms: relatedTerms,
+            needsUpdating: needsUpdatingDdElements.length > 0,
+            lastUpdatedDate: lastUpdatedDate
+        }
+    }
+
+    function glossaryItemTermFromDtElement(dtElement) {
+        const dfnElement = dtElement.querySelector('dfn');
+        const id = dfnElement.id || null;
+        const isAbbreviation = Boolean(dfnElement.querySelector('abbr'));
+        const body = dfnElement.textContent;
+
+        return {
+            id: normaliseWhitespace(id),
+            isAbbreviation: isAbbreviation,
+            body: normaliseWhitespace(body)
         };
-    });
-}
-
-function reflectThemeInClassList() {
-    if (localStorage.glossaryPageTheme === 'dark' || (!('glossaryPageTheme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark');
-    } else {
-        document.documentElement.classList.remove('dark');
-    }
-}
-
-const app = Elm.ApplicationShell.init({
-    flags: {
-        windowLocationHref: window.location.href,
-        titleString: normaliseWhitespace(titleElement.textContent),
-        aboutParagraph: aboutParagraph,
-        aboutLinks: aboutLinks,
-        glossaryItems: glossaryItems,
-        editorIsRunning: editorIsRunning,
-        enableHelpForMakingChanges: enableHelpForMakingChanges,
-        enableSavingChangesInMemory: enableSavingChangesInMemory,
-        enableExportMenu: enableExportMenu,
-        enableMarkdownBasedSyntax: enableMarkdownBasedSyntax,
-        enableLastUpdatedDates: enableLastUpdatedDates,
-        theme: localStorage.glossaryPageTheme || 'system',
-        orderItemsBy: localStorage.orderItemsBy || 'alphabetically',
-        cardWidth: cardWidth,
-        katexIsAvailable: katexIsAvailable
-    }
-});
-
-app.ports.allowBackgroundScrolling.subscribe(() => {
-    document.querySelector('body').classList.toggle('overflow-hidden', false);
-});
-
-app.ports.preventBackgroundScrolling.subscribe(() => {
-    document.querySelector('body').classList.toggle('overflow-hidden', true);
-});
-
-app.ports.scrollElementIntoView.subscribe((elementId) => {
-    document.getElementById(elementId).scrollIntoView({ block: "nearest" });
-});
-
-app.ports.scrollSearchResultIntoView.subscribe((elementId) => {
-    document.getElementById(elementId).scrollIntoView({ block: "nearest" });
-});
-
-app.ports.changeTheme.subscribe((themeName) => {
-    if (themeName) {
-        localStorage.glossaryPageTheme = themeName;
-    } else {
-        localStorage.removeItem('glossaryPageTheme');
     }
 
-    reflectThemeInClassList();
-});
+    function glossaryItemRelatedTermFromDdElement(ddElement) {
+        return Array.prototype.slice.apply(ddElement.querySelectorAll('a')).map(aElement => {
+            const hrefAttribute = aElement.getAttribute('href');
 
-app.ports.changeOrderItemsBy.subscribe((orderItemsBy) => {
-    localStorage.orderItemsBy = orderItemsBy;
-});
+            return {
+                idReference: hrefAttribute.substring(1),
+                body: normaliseWhitespace(aElement.textContent)
+            };
+        });
+    }
 
-app.ports.getCurrentDateTimeForSaving.subscribe(() => {
-    app.ports.receiveCurrentDateTimeForSaving.send(new Date().toISOString());
-});
+    function reflectThemeInClassList() {
+        if (localStorage.glossaryPageTheme === 'dark' || (!('glossaryPageTheme' in localStorage) &&
+            window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }
 
-function domReady(callback) {
-    document.readyState === 'interactive' || document.readyState === 'complete' ?
-        callback() : document.addEventListener('DOMContentLoaded', callback);
-}
-
-// Prevent FOUC
-domReady(() => {
-    reflectThemeInClassList();
-
-    document.body.style.visibility = 'visible';
-
-    // Try to keep the focus on the element that receives overall keyboard shortcuts like Control-K
-    document.getElementById("glossary-page-outer")?.focus();
-
-    document.addEventListener("focusout", (e) => {
-        if (e.relatedTarget == null) {
-            document.getElementById("glossary-page-outer")?.focus();
+    const app = Elm.ApplicationShell.init({
+        flags: {
+            windowLocationHref: window.location.href,
+            titleString: normaliseWhitespace(titleElement.textContent),
+            aboutParagraph: aboutParagraph,
+            aboutLinks: aboutLinks,
+            glossaryItems: glossaryItems,
+            editorIsRunning: editorIsRunning,
+            enableHelpForMakingChanges: enableHelpForMakingChanges,
+            enableSavingChangesInMemory: enableSavingChangesInMemory,
+            enableExportMenu: enableExportMenu,
+            enableMarkdownBasedSyntax: enableMarkdownBasedSyntax,
+            enableLastUpdatedDates: enableLastUpdatedDates,
+            theme: localStorage.glossaryPageTheme || 'system',
+            orderItemsBy: localStorage.orderItemsBy || 'alphabetically',
+            cardWidth: cardWidth,
+            katexIsAvailable: katexIsAvailable
         }
     });
 
-    const browserLocale =
-        navigator.languages && navigator.languages.length
-            ? navigator.languages[0]
-            : navigator.language;
+    app.ports.allowBackgroundScrolling.subscribe(() => {
+        document.querySelector('body').classList.toggle('overflow-hidden', false);
+    });
 
-    const lastUpdatedDateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
+    app.ports.preventBackgroundScrolling.subscribe(() => {
+        document.querySelector('body').classList.toggle('overflow-hidden', true);
+    });
 
-    customElements.define('last-updated',
-        class extends HTMLElement {
-            constructor() { super(); }
-            connectedCallback() { this.setTextContent(); }
-            attributeChangedCallback() { this.setTextContent(); }
-            static get observedAttributes() { return ['datetime']; }
+    app.ports.scrollElementIntoView.subscribe((elementId) => {
+        document.getElementById(elementId).scrollIntoView({ block: "nearest" });
+    });
 
-            setTextContent() {
-                const datetimeAttribute = this.getAttribute('datetime');
+    app.ports.scrollSearchResultIntoView.subscribe((elementId) => {
+        document.getElementById(elementId).scrollIntoView({ block: "nearest" });
+    });
 
-                if (datetimeAttribute)
-                    this.textContent = new Date(datetimeAttribute).toLocaleDateString(browserLocale, lastUpdatedDateOptions);
-                else
-                    this.textContent = "unknown";
+    app.ports.changeTheme.subscribe((themeName) => {
+        if (themeName) {
+            localStorage.glossaryPageTheme = themeName;
+        } else {
+            localStorage.removeItem('glossaryPageTheme');
+        }
+
+        reflectThemeInClassList();
+    });
+
+    app.ports.changeOrderItemsBy.subscribe((orderItemsBy) => {
+        localStorage.orderItemsBy = orderItemsBy;
+    });
+
+    app.ports.getCurrentDateTimeForSaving.subscribe(() => {
+        app.ports.receiveCurrentDateTimeForSaving.send(new Date().toISOString());
+    });
+
+    function domReady(callback) {
+        document.readyState === 'interactive' || document.readyState === 'complete' ?
+            callback() : document.addEventListener('DOMContentLoaded', callback);
+    }
+
+    // Prevent FOUC
+    domReady(() => {
+        reflectThemeInClassList();
+
+        document.body.style.visibility = 'visible';
+
+        // Try to keep the focus on the element that receives overall keyboard shortcuts like Control-K
+        document.getElementById("glossary-page-outer")?.focus();
+
+        document.addEventListener("focusout", (e) => {
+            if (e.relatedTarget == null) {
+                document.getElementById("glossary-page-outer")?.focus();
             }
-        }
-    );
+        });
 
-    if (katexIsAvailable) {
-        customElements.define('katex-inline',
+        const browserLocale =
+            navigator.languages && navigator.languages.length
+                ? navigator.languages[0]
+                : navigator.language;
+
+        const lastUpdatedDateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
+
+        customElements.define('last-updated',
             class extends HTMLElement {
                 constructor() { super(); }
                 connectedCallback() { this.setTextContent(); }
                 attributeChangedCallback() { this.setTextContent(); }
-                static get observedAttributes() { return ['data-expr']; }
+                static get observedAttributes() { return ['datetime']; }
 
                 setTextContent() {
-                    katex.render(this.dataset.expr, this, {
-                        throwOnError: false
-                    });
+                    const datetimeAttribute = this.getAttribute('datetime');
 
+                    if (datetimeAttribute)
+                        this.textContent = new Date(datetimeAttribute).toLocaleDateString(browserLocale, lastUpdatedDateOptions);
+                    else
+                        this.textContent = "unknown";
                 }
             }
         );
 
-        customElements.define('katex-display',
-            class extends HTMLElement {
-                constructor() { super(); }
-                connectedCallback() { this.setTextContent(); }
-                attributeChangedCallback() { this.setTextContent(); }
-                static get observedAttributes() { return ['data-expr']; }
+        if (katexIsAvailable) {
+            customElements.define('katex-inline',
+                class extends HTMLElement {
+                    constructor() { super(); }
+                    connectedCallback() { this.setTextContent(); }
+                    attributeChangedCallback() { this.setTextContent(); }
+                    static get observedAttributes() { return ['data-expr']; }
 
-                setTextContent() {
-                    katex.render(this.dataset.expr, this, {
-                        displayMode: true,
-                        throwOnError: false
-                    });
+                    setTextContent() {
+                        katex.render(this.dataset.expr, this, {
+                            throwOnError: false
+                        });
+
+                    }
                 }
-            }
-        );
-    }
-});
+            );
+
+            customElements.define('katex-display',
+                class extends HTMLElement {
+                    constructor() { super(); }
+                    connectedCallback() { this.setTextContent(); }
+                    attributeChangedCallback() { this.setTextContent(); }
+                    static get observedAttributes() { return ['data-expr']; }
+
+                    setTextContent() {
+                        katex.render(this.dataset.expr, this, {
+                            displayMode: true,
+                            throwOnError: false
+                        });
+                    }
+                }
+            );
+        }
+    });
+}
