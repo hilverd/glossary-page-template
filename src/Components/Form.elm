d826c852c48887a086e5d82cf8dcd8f9ee7f1083
Hilverd Reker <hilverd@hilverdreker.com>
2023-11-22 20:04:41 +0000

    Improve styling

--- src/Components/Form.elm
+++ src/Components/Form.elm
@@ -71,7 +71,7 @@ textarea body markdownBasedSyntaxEnabled mathSupportEnabled showValidationErrors
             ]
             [ Accessibility.textarea
                 ([ if not showValidationErrors || validationError == Nothing then
-                    class "shadow-sm w-full rounded-md border border-gray-300 dark:border-gray-500 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:text-white"
+                    class "shadow-sm w-full rounded-md border border-gray-300 dark:border-gray-500 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:text-white placeholder-gray-500 dark:placeholder-gray-400"
 
                    else
                     class "shadow-sm w-full rounded-md border-red-300 text-red-900 dark:text-red-300 placeholder-red-300 dark:placeholder-red-700 focus:outline-none focus:ring-red-500 focus:border-red-500 dark:bg-gray-700"
