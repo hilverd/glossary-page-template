5e659721feeaf530318b61d2e3320d26362ab350
Hilverd Reker <hilverd@hilverdreker.com>
2023-12-30 21:18:19 +0000

    Send GlossaryChanges JSON if separate backend is being used

--- src/Save.elm
+++ src/Save.elm
@@ -9,9 +9,10 @@ module Save exposing (changeAndSave)
 
 -}
 
+import Codec
 import Data.Editability exposing (Editability(..))
 import Data.Glossary as Glossary exposing (Glossary)
-import Data.GlossaryChanges exposing (GlossaryChanges)
+import Data.GlossaryChanges as GlossaryChanges exposing (GlossaryChanges)
 import Data.GlossaryItemId exposing (GlossaryItemId)
 import Data.Saving exposing (Saving(..))
 import Extras.HtmlTree as HtmlTree
@@ -38,6 +39,9 @@ changeAndSave editability glossary changes errorMsg successMsg =
                 EditingWithIncludedBackend ->
                     ( SavingInProgress, patchHtmlFile resultOfApplyingChanges errorMsg successMsg )
 
+                EditingWithSeparateBackend baseUrl ->
+                    ( SavingInProgress, sendChangesAsPatch baseUrl changes resultOfApplyingChanges errorMsg successMsg )
+
                 _ ->
                     ( NotCurrentlySaving, Cmd.none )
 
@@ -60,6 +64,37 @@ patchHtmlFile ( maybeGlossaryItemId, glossary ) errorMsg successMsg =
                 |> Glossary.toHtmlTree
                 |> HtmlTree.toHtmlReplacementString
                 |> Http.stringBody "text/html"
+        , expect =
+            Http.expectWhatever
+                (\result ->
+                    case result of
+                        Ok _ ->
+                            successMsg ( maybeGlossaryItemId, glossary )
+
+                        Err error ->
+                            errorMsg error
+                )
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+sendChangesAsPatch :
+    String
+    -> GlossaryChanges
+    -> ( Maybe GlossaryItemId, Glossary )
+    -> (Http.Error -> msg)
+    -> (( Maybe GlossaryItemId, Glossary ) -> msg)
+    -> Cmd msg
+sendChangesAsPatch baseUrl changes ( maybeGlossaryItemId, glossary ) errorMsg successMsg =
+    Http.request
+        { method = "PATCH"
+        , headers = []
+        , url = baseUrl
+        , body =
+            changes
+                |> Codec.encodeToValue GlossaryChanges.codec
+                |> Http.jsonBody
         , expect =
             Http.expectWhatever
                 (\result ->
