36bbd5ed3e665476f4c8460698fdef447289bc04
Hilverd Reker <hilverd@hilverdreker.com>
2024-04-20 21:02:44 +0100

    Document constraints to test for GlossaryFromDom

--- tests/Data/GlossaryFromDomTests.elm
+++ tests/Data/GlossaryFromDomTests.elm
@@ -23,3 +23,17 @@ suite =
                     |> Codec.decodeString GlossaryFromDom.codec
                     |> Expect.equal (Ok TestData.glossaryFromDom)
         ]
+
+
+
+{-
+
+   TODO Test these constraints:
+
+   * a term cannot start with "glossary-page-" (after changing to lowercase)
+   * no two distinct items can have the same disambiguated preferred term (fragment) identifier
+   * an item's disambiguated preferred term (fragment) identifier cannot be the same as the (fragment) identifier for an alternative term in any item
+   * an item's preferred term (fragment) identifier cannot be the same as (fragment) identifier for an alternative term in any item
+   * no two distinct items can share an alternative term (fragment) identifier
+
+-}
