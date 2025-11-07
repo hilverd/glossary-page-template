7c7043ea895a2d5e6bc1a1daac6c4699a9e7e1a9
Hilverd Reker <hilverd@hilverdreker.com>
2025-11-07 21:11:41 +0000

    Tweak layout

--- src/Data/GlossaryItem/Term.elm
+++ src/Data/GlossaryItem/Term.elm
@@ -172,7 +172,7 @@ view enableMathSupport showTagIcon additionalAttributes (MarkdownTerm { body }) 
                         (class "prose dark:prose-invert print:prose-neutral dark:prose-pre:text-gray-200 prose-code:before:hidden prose-code:after:hidden leading-normal" :: additionalAttributes)
                         (if showTagIcon then
                             Icons.tag
-                                [ Svg.Attributes.class "h-4 w-4 mr-1 text-gray-400 dark:text-gray-300 inline-block flex-shrink-0" ]
+                                [ Svg.Attributes.class "h-4 w-4 mr-1.5 text-gray-400 dark:text-gray-300 inline-block flex-shrink-0" ]
                                 :: rendered
 
                          else
